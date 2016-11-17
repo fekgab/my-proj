f269f316ed5a8a13a32dc2b1dad74e3c8d9ae8c8
gabor_fekete <gabor_fekete1@epam.com>
2016-11-17 14:42:25 +0100

    checkpont-20141117

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,20 +1,43 @@
-import { NgModule }      from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
-import { AppComponent }  from './app.component';
+
+import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import { DashboardComponent } from './dashboard.component'
+
+import { HeroService } from './hero.service';
 
 
 @NgModule({
-  imports: [ 
+  imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: 'heroes',
+        component: HeroesComponent
+      },
+      {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+      },
+    ])
   ],
-  declarations: [ 
+  declarations: [
     AppComponent,
-    HeroDetailComponent
-   ],
-  bootstrap:    [ AppComponent ]
+    DashboardComponent,
+    HeroDetailComponent,
+    HeroesComponent
+  ],
+  providers: [
+    HeroService,
+  ],
+  bootstrap: [AppComponent]
 })
+
 export class AppModule { }
